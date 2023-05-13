96a7cac8b715e8f5921cdc7461d5efc31553c4cc
Dima <ddd12@mail.ru>
2023-05-13 21:26:04 +0300

    Task_4

---
+++ eureka-config/src/main/java/ru/orlov/micro/eurekaconfig/EurekaConfigApplication.java
@@ -0,0 +1,13 @@
+package ru.orlov.micro.eurekaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EurekaConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigApplication.class, args);
+    }
+}
---
+++ eureka-config/src/test/java/ru/orlov/micro/eurekaconfig/EurekaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.orlov.micro.eurekaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
